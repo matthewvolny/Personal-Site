17d566c045bb406ba243b0354f89ba963fe98816
matthewvolny <matthew.volny@gmail.com>
2022-05-06 09:30:37 -0400

    added scroll images functionality

--- scripts.js
+++ scripts.js
@@ -10,3 +10,54 @@ function addInView() {
 }
 
 window.addEventListener("scroll", addInView);
+
+const slideshowImages = document.querySelectorAll(".slideshow-image");
+const slideshowNavBarDots = document.querySelectorAll(
+  ".slideshow-nav-bar > div"
+);
+
+let currentIndex = 0;
+let prevIndex = null;
+
+slideshowImages.forEach((slide, index) => {
+  if (index > currentIndex) {
+    slide.style.display = "none";
+  }
+});
+
+const changeSlide = () => {
+  slideshowImages.forEach((slide, index) => {
+    if (index < currentIndex || index > currentIndex) {
+      slide.style.display = "none";
+    } else {
+      slide.style.display = "inline-block";
+    }
+  });
+};
+
+for (let i = 0; i < slideshowNavBarDots.length; i++) {
+  slideshowNavBarDots[i].addEventListener("click", () => {
+    if (prevIndex !== null) {
+      slideshowNavBarDots[prevIndex].removeAttribute("id");
+    }
+    slideshowNavBarDots[i].setAttribute("id", "nav-dot-clicked");
+    console.log(i);
+    currentIndex = i;
+    changeSlide();
+    prevIndex = i;
+  });
+}
+
+// slideshowNavBarDots.forEach((dot, index) => {
+//   dot.addEventListener("click", () => {
+//     if (dot[index - 1].id) {
+//       dot[index - 1].removeAttribute("id");
+//     } else if (dot[index + 1].id) {
+//       dot[index + 1].removeAttribute("id");
+//     }
+//     dot.setAttribute("id", "nav-dot-clicked");
+//     console.log(index);
+//     currentIndex = index;
+//     changeSlide();
+//   });
+// });
