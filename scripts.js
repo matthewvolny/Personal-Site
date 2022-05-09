0f51fcac2673f568b7a03d9ac85c6b3b7db5dbe4
matthewvolny <matthew.volny@gmail.com>
2022-05-09 09:40:02 -0400

    added images for chat app and educational app

--- scripts.js
+++ scripts.js
@@ -84,7 +84,16 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
   },
+
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
