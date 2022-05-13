869e4b8bec6ee8bbb27ed5e7208a006913319e7e
matthewvolny <matthew.volny@gmail.com>
2022-05-13 13:49:21 -0400

    many style changes related to the project description and image slideshow, added project resize script

--- scripts.js
+++ scripts.js
@@ -93,7 +93,7 @@ var swiper = new Swiper(".mySwiper", {
   //   el: ".swiper-pagination",
   //   clickable: true,
   // },
-
+  // zoom: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -140,3 +140,125 @@ for (let i = 0; i < navDropdownItems.length; i++) {
 const siteLinksSmallerWidths = document.querySelectorAll(
   "#site-links-smaller-widths"
 );
+
+//
+//
+//
+let lastWindowWidth;
+// let lastSlideShowContainerWidth;
+let slideShowContainerRatio = 1.24;
+
+const scaleSlideShowContainer = (width) => {
+  if (width <= "935") {
+    let slideShowContainer = document.querySelector(
+      ".project-odd > .project-image-container"
+    );
+    let slideShowContainerHeight = slideShowContainer.offsetHeight;
+    let slideShowContainerWidth = slideShowContainer.offsetWidth;
+    console.log("slideShowContainerHeight");
+    console.log(slideShowContainerHeight);
+    console.log("slideShowContainerWidth");
+    console.log(slideShowContainerWidth);
+    let newHeight = slideShowContainerWidth / slideShowContainerRatio;
+    console.log("newHeight");
+    console.log(newHeight);
+    slideShowContainer.style.height = `${newHeight}px`;
+    lastWindowWidth = width;
+    // console.log("lastWindowWidth last log");
+    // console.log(lastWindowWidth);
+  } else {
+    lastWindowWidth = width;
+    //!or standard ratio
+    // lastSlideShowContainerWidth = slideShowContainer.offsetWidth;
+  }
+};
+
+const scaleProjectImageContainer = (currentWidth) => {
+  if (currentWidth >= 986 && currentWidth <= 1275) {
+    const projectImageContainer = document.querySelector(
+      ".project-odd > .project-image-container"
+    );
+    const body = document.querySelector("body");
+    const projectImageContainerWidth = projectImageContainer.offsetWidth;
+    const bodyWidth = body.offsetWidth;
+    const projectImageDiff = bodyWidth - projectImageContainerWidth;
+    console.log("projectImageDiff");
+    console.log(projectImageDiff);
+    const projectDescriptionDiff =
+      bodyWidth -
+      document.querySelector(".project-odd > .project-details").offsetWidth;
+    console.log("projectDescriptionDiff");
+    console.log(projectDescriptionDiff);
+    projectImageContainer.style.marginLeft = `-${
+      600 - (projectImageDiff - /*projectDescriptionDiff*/ 52)
+    }px`;
+  } else {
+  }
+  // if (currentWidth >= 1276) {
+  //   const projectImageContainer = document.querySelector(
+  //     ".project-odd > .project-image-container"
+  //   );
+  //   const body = document.querySelector("body");
+  //   const projectImageContainerWidth = projectImageContainer.offsetWidth;
+  //   const bodyWidth = body.offsetWidth;
+  //   const projectImageDiff = bodyWidth - projectImageContainerWidth;
+  //   console.log("projectImageDiff");
+  //   console.log(projectImageDiff);
+  //   const projectDescriptionDiff =
+  //     bodyWidth -
+  //     document.querySelector(".project-odd > .project-details").offsetWidth;
+  //   console.log("projectDescriptionDiff");
+  //   console.log(projectDescriptionDiff);
+  //   projectImageContainer.style.marginLeft = `-${
+  //     600 - (projectImageDiff - /*projectDescriptionDiff*/ 100)
+  //   }px`;
+  // }
+};
+
+//sets page width to state!
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+var myEfficientFn = debounce(function () {
+  console.log(
+    // document.body.clientWidth +
+    //   " wide by " +
+    //   document.documentElement.clientHeight +
+    //   " high"
+    window.innerWidth
+  );
+  scaleSlideShowContainer(window.innerWidth);
+  scaleProjectImageContainer(window.innerWidth);
+}, 1);
+
+// window.addEventListener("resize", myEfficientFn);
+window.addEventListener("resize", () => {
+  scaleSlideShowContainer(window.innerWidth);
+  scaleProjectImageContainer(window.innerWidth);
+});
+
+// set sizes on page load
+scaleSlideShowContainer(window.innerWidth);
+scaleProjectImageContainer(window.innerWidth);
+
+//expand second sentence of project description on button click
+let expandProjectButton = document.querySelector(".expand-project-button");
+let secondProjectSentence = document.querySelector(".second-project-sentence");
+
+expandProjectButton.addEventListener("click", () => {
+  secondProjectSentence.style.display = "contents";
+  expandProjectButton.style.display = "none";
+});
