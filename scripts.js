a0379870ecbbdcfcfe5f6b83eade1393c6158f07
matthewvolny <matthew.volny@gmail.com>
2022-05-19 21:24:16 -0400

    added gradient map overlap image with animation

--- scripts.js
+++ scripts.js
@@ -11,16 +11,16 @@ function addInView() {
 
 window.addEventListener("scroll", addInView);
 
-const profileImage = document.querySelector(".profile-image-container > img");
-profileImage.addEventListener("mouseover", () => {
-  const imageBackground = document.querySelector(".profile-image-background");
-  imageBackground.setAttribute("id", "image-background-hovered");
-});
+// const profileImage = document.querySelector(".profile-image-container > img");
+// profileImage.addEventListener("mouseover", () => {
+//   const imageBackground = document.querySelector(".profile-image-background");
+//   imageBackground.setAttribute("id", "image-background-hovered");
+// });
 
-profileImage.addEventListener("mouseout", () => {
-  const imageBackground = document.querySelector(".profile-image-background");
-  imageBackground.removeAttribute("id");
-});
+// profileImage.addEventListener("mouseout", () => {
+//   const imageBackground = document.querySelector(".profile-image-background");
+//   imageBackground.removeAttribute("id");
+// });
 
 const slideshowImages = document.querySelectorAll(".slideshow-image");
 const slideshowNavBarDots = document.querySelectorAll(
@@ -36,42 +36,28 @@ slideshowImages.forEach((slide, index) => {
   }
 });
 
-const changeSlide = () => {
-  slideshowImages.forEach((slide, index) => {
-    if (index < currentIndex || index > currentIndex) {
-      slide.style.display = "none";
-    } else {
-      slide.style.display = "inline-block";
-    }
-  });
-};
-
-for (let i = 0; i < slideshowNavBarDots.length; i++) {
-  slideshowNavBarDots[i].addEventListener("click", () => {
-    if (prevIndex !== null) {
-      slideshowNavBarDots[prevIndex].removeAttribute("id");
-    }
-    slideshowNavBarDots[i].setAttribute("id", "nav-dot-clicked");
-    console.log(i);
-    currentIndex = i;
-    changeSlide();
-    prevIndex = i;
-  });
-}
-
-// slideshowNavBarDots.forEach((dot, index) => {
-//   dot.addEventListener("click", () => {
-//     if (dot[index - 1].id) {
-//       dot[index - 1].removeAttribute("id");
-//     } else if (dot[index + 1].id) {
-//       dot[index + 1].removeAttribute("id");
+// const changeSlide = () => {
+//   slideshowImages.forEach((slide, index) => {
+//     if (index < currentIndex || index > currentIndex) {
+//       slide.style.display = "none";
+//     } else {
+//       slide.style.display = "inline-block";
 //     }
-//     dot.setAttribute("id", "nav-dot-clicked");
-//     console.log(index);
-//     currentIndex = index;
-//     changeSlide();
 //   });
-// });
+// };
+
+// for (let i = 0; i < slideshowNavBarDots.length; i++) {
+//   slideshowNavBarDots[i].addEventListener("click", () => {
+//     if (prevIndex !== null) {
+//       slideshowNavBarDots[prevIndex].removeAttribute("id");
+//     }
+//     slideshowNavBarDots[i].setAttribute("id", "nav-dot-clicked");
+//     console.log(i);
+//     currentIndex = i;
+//     changeSlide();
+//     prevIndex = i;
+//   });
+// }
 
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
@@ -363,3 +349,22 @@ for (let i = 0; i < footerTopIconImages.length; i++) {
     footerBottomIconImages[i].removeAttribute("id");
   });
 }
+
+// animate photo
+const myPhotoContainer = document.querySelector(".my-photo-container");
+const myPhoto = document.querySelector(".my-photo");
+const myPhotoOverlay = document.querySelector(".my-photo-overlay");
+const profileImageBackground = document.querySelector(
+  ".profile-image-background"
+);
+myPhotoContainer.addEventListener("mouseover", () => {
+  console.log("mouse entered");
+  myPhotoOverlay.setAttribute("id", "photo-overlay-hovered");
+  profileImageBackground.setAttribute("id", "profile-background-hovered");
+});
+
+myPhotoContainer.addEventListener("mouseout", () => {
+  console.log("mouse left");
+  myPhotoOverlay.removeAttribute("id");
+  profileImageBackground.removeAttribute("id");
+});
